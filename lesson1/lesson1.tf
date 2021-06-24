43009ab7b968f36475d3cf7490746610f6322e3f
tehnik <tehnik@adps.dpsu>
2021-06-24 16:46:38 +0300

    add tags

--- lesson1/lesson1.tf
+++ lesson1/lesson1.tf
@@ -7,8 +7,14 @@ resource "aws_instance" "ubuntu" {
   count = 2
   ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
+  tags = {
+    name = Ubuntu_20.04
+  }
 }
 resource "aws_instance" "amazonLinux" {
   ami = "ami-0ab4d1e9cf9a1215a"
   instance_type = "t2.micro"
+  tags = {
+    name = Amazon_Linux
+  }
 }
