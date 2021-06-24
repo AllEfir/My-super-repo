7f4ea063140937d94a51dc152ab01f24771b935c
tehnik <tehnik@adps.dpsu>
2021-06-24 16:31:27 +0300

    add 2 ubuntu(with count) and 1amazonLinux

--- lesson1/lesson1.tf
+++ lesson1/lesson1.tf
@@ -4,6 +4,11 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "ubuntu" {
+  count = 2
   ami = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+}
+resource "aws_instance" "amazonLinux" {
+  ami = "ami-0ab4d1e9cf9a1215a"
   instance_type = "t2.micro"
 }
