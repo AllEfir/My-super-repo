d80ed8ef515053dc461bf348713428e34eff8b78
tehnik <tehnik@adps.dpsu>
2021-06-24 16:53:00 +0300

    delete credentials from code

--- lesson1/lesson1.tf
+++ lesson1/lesson1.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  access_key = "AKIAQOJBM7CEOJYSPAFX"
-  secret_key = "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"
-  region = "us-east-1"
-}
+provider "aws" {}
 resource "aws_instance" "ubuntu" {
   count = 2
   ami = "ami-09e67e426f25ce0d7"
