22823cc379d4ede39455334fc95c5ba1bcf52b8f
tehnik <tehnik@adps.dpsu>
2021-06-26 20:06:34 +0300

    split tf files: main, provider, variables

---
+++ lesson5/main.tf
@@ -0,0 +1,51 @@
+resource "aws_instance" "my_webserver" {
+  ami = "ami-089b5384aac360007" # Amazon Linux AMI
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  key_name      = "key_makentosh"
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Nikita",
+    l_name = "Olefir",
+    names = ["vasya", "kolya", "john", "donald", "maksim"]
+  })
+
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "security group "
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+    Region = var.aws_region
+  }
+}
