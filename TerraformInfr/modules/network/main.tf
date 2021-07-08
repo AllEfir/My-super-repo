3b56d5dcde015f5d073573df13a46b2c9a3bb049
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-07-08 21:14:59 +0300

    created gateway and route

--- TerraformInfr/modules/network/main.tf
+++ TerraformInfr/modules/network/main.tf
@@ -9,6 +9,26 @@ resource "aws_vpc" "VirtualPrivateCloud" {
     CidrBlock = "172.16.1.0/24"
   }
 }
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.VirtualPrivateCloud.id
+  tags = {
+    name = "gateway"
+  }
+}
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.VirtualPrivateCloud.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+  tags = {
+    name = "Route table"
+  }
+}
+resource "aws_route_table_association" "route_association" {
+  route_table_id = aws_route_table.route.id
+  subnet_id = aws_subnet.subnet.id
+}
 resource "aws_subnet" "subnet" {
   cidr_block = "172.16.1.0/24"
   vpc_id = aws_vpc.VirtualPrivateCloud.id
