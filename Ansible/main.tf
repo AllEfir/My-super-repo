18b539b24f217956c189972d68410a43955f5f8b
tehnik <tehnik@adps.dpsu>
2021-07-16 01:34:44 +0300

    created ansible-master-client,adaptive for terraform-cloud,need fix for ansible-clients

--- Ansible/main.tf
+++ Ansible/main.tf
@@ -3,7 +3,50 @@ resource "aws_instance" "ansible-master" {
   instance_type = "t2.micro"
   key_name = "makentosh-key"
   vpc_security_group_ids = [aws_security_group.securitygroup.id]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo yum install python3-pip -y",
+      "sudo pip install ansible -y",
+    ]
+  }
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = var.aws_private_key
+  }
+  tags = {
+    Name = "ansible master client"
+  }
 }
+/*resource "aws_instance" "ansible-client1" {
+  ami = "ami-00f22f6155d6d92c5"
+  instance_type = "t2.micro"
+  key_name = "key_makentosh"
+  vpc_security_group_ids = [aws_security_group.securitygroup.id]
+  tags = {
+    Name = "client for ansible1"
+  }
+}
+resource "aws_instance" "ansible-client2" {
+  ami = "ami-00f22f6155d6d92c5"
+  instance_type = "t2.micro"
+  key_name = "makentosh-key"
+  vpc_security_group_ids = [aws_security_group.securitygroup.id]
+  tags = {
+    Name = "client for ansible2"
+  }
+}
+resource "aws_instance" "ansible-client3" {
+  ami = "ami-00f22f6155d6d92c5"
+  instance_type = "t2.micro"
+  key_name = "makentosh-key"
+  vpc_security_group_ids = [aws_security_group.securitygroup.id]
+  tags = {
+    Name = "client for ansible3"
+  }
+}*/
 resource "aws_security_group" "securitygroup" {
   ingress {
     description      = "HTTP"
