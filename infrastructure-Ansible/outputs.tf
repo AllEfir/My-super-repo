40726b8c47d393cff3f9e888407497a39dbb1393
tehnik <emailexampl@mail>
2021-07-26 20:49:48 +0300

    some fix main.tf

--- infrastructure-Ansible/outputs.tf
+++ infrastructure-Ansible/outputs.tf
@@ -5,11 +5,11 @@ output "ip-Ansible-master-public-ip" {
   value = aws_instance.ansible-master.public_ip
 }*/
 output "ip-nginx-private-ip" {
-  value = aws_instance.nginx.private_ip
+  value = aws_instance.ansible-client.private_ip
 }
 output "ip-nginx-public-ip" {
-  value = aws_instance.nginx.public_ip
+  value = aws_instance.ansible-client.public_ip
 }
 output "id-security-group-for-ansible-master-and-nginx" {
-  value = aws_security_group.securitygroupNGINX.id
+  value = aws_security_group.securitygroupAnsible-client.id
 }
