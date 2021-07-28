760ca92656a91fe8d678bc478d4dff0e4f7fb975
tehnik <emailexampl@mail>
2021-07-28 20:43:00 +0300

    add 2jenkins slaves

--- infrastructure-Ansible/main.tf
+++ infrastructure-Ansible/main.tf
@@ -46,28 +46,26 @@ resource "aws_instance" "ansible-client" {
     private_key = var.aws_private_key
   }*/
 }
-/*resource "aws_instance" "ansible-client-test" {
+resource "aws_instance" "jenkins-slave-1" {
   ami = "ami-089b5384aac360007"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.securitygroupAnsible-client.id]
   key_name = "makentosh-key"
   tags = {
-    Name = "ansible-client-test"
+    Name = "jenkins-slave-1"
     deskriptn = "server for deploy  with ansible"
-    env = "test"
   }
 }
-resource "aws_instance" "ansible-client-prod" {
+resource "aws_instance" "jenkins-slave-2" {
   ami = "ami-089b5384aac360007"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.securitygroupAnsible-client.id]
   key_name = "makentosh-key"
   tags = {
-    Name = "ansible-client-test"
+    Name = "jenkins-slave-2"
     deskriptn = "server for deploy  with ansible"
-    env = "prod"
   }
-}*/
+}
 
 resource "aws_security_group" "securitygroupAnsible-client" {
   name = "Security Group for ansible-client"
